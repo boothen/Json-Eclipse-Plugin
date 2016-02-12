616b9de81deaa99b93550ba373b41c59fba4cb60
Martin Steiger <git@martin-steiger.de>
2016-02-12 21:17:06 +0100

    Add New File Wizard

---
+++ jsonedit-editor/src/main/java/com/boothen/jsonedit/editor/JsonNewFileWizard.java
@@ -0,0 +1,71 @@
+package com.boothen.jsonedit.editor;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+/**
+ * A file wizard for JSON files.
+ */
+public class JsonNewFileWizard extends BasicNewResourceWizard
+{
+    private WizardNewFileCreationPage mainPage;
+
+    public JsonNewFileWizard()
+    {
+        super();
+    }
+
+    @Override
+    public void addPages()
+    {
+        super.addPages();
+        mainPage = new WizardNewFileCreationPage("newFilePage1", getSelection());
+        mainPage.setTitle("Create JSON File");
+        mainPage.setFileName("file.json");
+        addPage(mainPage);
+    }
+
+    @Override
+    public void init(IWorkbench workbench, IStructuredSelection currentSelection)
+    {
+        super.init(workbench, currentSelection);
+        setWindowTitle("New JSON File");
+        setNeedsProgressMonitor(false);
+    }
+
+    @Override
+    public boolean performFinish()
+    {
+        IFile file = mainPage.createNewFile();
+        if (file == null) {
+            return false;
+        }
+
+        selectAndReveal(file);
+
+        final IWorkbenchWindow dw = getWorkbench().getActiveWorkbenchWindow();
+
+        try {
+            if (dw != null) {
+                IWorkbenchPage page = dw.getActivePage();
+
+                if (page != null) {
+                    IDE.openEditor(page, file, true);
+                }
+            }
+        }
+        catch (PartInitException e) {
+            StatusManager.getManager().handle(e.getStatus());
+        }
+
+        return true;
+    }
+}
