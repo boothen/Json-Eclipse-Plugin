a01c120795eee8f852b51ce85deabd9df72f3319
Martin Steiger <git@martin-steiger.de>
2016-02-14 20:19:16 +0100

    Add auto-generated JSON parser

---
+++ jsonedit-model/src/main/java/com/boothen/jsonedit/antlr/JSONBaseListener.java
@@ -0,0 +1,99 @@
+// Generated from JSON.g4 by ANTLR 4.5.2
+package com.boothen.jsonedit.antlr;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link JSONListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class JSONBaseListener implements JSONListener {
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterJson(JSONParser.JsonContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitJson(JSONParser.JsonContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterObject(JSONParser.ObjectContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitObject(JSONParser.ObjectContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterPair(JSONParser.PairContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitPair(JSONParser.PairContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterArray(JSONParser.ArrayContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitArray(JSONParser.ArrayContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterValue(JSONParser.ValueContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitValue(JSONParser.ValueContext ctx) { /* empty */ }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterEveryRule(ParserRuleContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitEveryRule(ParserRuleContext ctx) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitTerminal(TerminalNode node) { /* empty */ }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitErrorNode(ErrorNode node) { /* empty */ }
+}
