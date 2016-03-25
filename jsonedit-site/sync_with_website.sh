1724dd29e04f6410a216d12c52154a050188ca3c
Martin Steiger <git@martin-steiger.de>
2016-03-25 17:14:40 +0100

    Replace disfunctional XSLT transformation with Magic XML

--- jsonedit-site/sync_with_website.sh
+++ jsonedit-site/sync_with_website.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SITE="."
 
-scp -p -r $SITE/*.jar $SITE/site.xml $SITE/features $SITE/plugins $SITE/web $SITE/index.html martin-steiger.de:/var/www/html/jsonedit
+scp -p -r $SITE/*.jar $SITE/site.xml $SITE/features $SITE/plugins $SITE/web $SITE/index.html $SITE/scripts martin-steiger.de:/var/www/html/jsonedit
